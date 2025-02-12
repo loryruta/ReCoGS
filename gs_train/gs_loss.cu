4b3dd2233a9e7ba2622a27af20573ca4ef628b9e
Lorenzo Rutayisire <loryruta23@gmail.com>
2025-02-12 15:27:48 +0100

    Add gs_loss

---
+++ gs_train/gs_loss.cu
@@ -0,0 +1,106 @@
+#include "gs_loss.h"
+
+#include "utils/misc_utils.h"
+
+#define FUSEDSSIM_C1 (0.01f * 0.01f)
+#define FUSEDSSIM_C2 (0.03f * 0.03f)
+
+#define NUM_THREADS 1024
+
+using namespace gs_train;
+
+GSLoss::GSLoss()
+{
+    m_tmp_buffer.resize(3 * sizeof(float));
+
+    m_L1_avg = m_tmp_buffer.data_ptr<float>();
+    m_Ldssim_avg = m_tmp_buffer.data_ptr<float>() + 1;
+    m_loss = m_tmp_buffer.data_ptr<float>() + 2;
+}
+
+__global__ void forward_kernel_1( //
+    size_t BCHW,
+    const float* img_pred,
+    const float* img_gt,
+    const float* ssim_map,
+    float* out_L1_avg,
+    float* out_Ldssim_avg)
+{
+    uint32_t i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i >= BCHW) return;
+
+    float abs = std::abs(img_pred[i] - img_gt[i]);
+
+    atomicAdd(out_L1_avg, abs / float(BCHW));
+    atomicAdd(out_Ldssim_avg, ssim_map[i] / float(BCHW)); // TODO not sure about ssim_map indexing
+}
+
+__global__ void forward_kernel_2( //
+    float lambda,
+    const float* L1_avg,
+    const float* Ldssim_avg,
+    float* out_loss)
+{
+    *out_loss = (1.0f - lambda) * (*L1_avg) ;//+ lambda * (*Ldssim_avg); TODO DEBUG
+}
+
+float* GSLoss::forward(int B, int C, int H, int W, const float* img_pred, const float* img_gt)
+{
+    float* ssim_map = m_fused_ssim.forward(FUSEDSSIM_C1, FUSEDSSIM_C2, B, C, H, W, img_pred, img_gt, true /* train */);
+    size_t BCHW = B * C * H * W;
+    dim3 num_blocks = div_ceil(BCHW, size_t(NUM_THREADS));
+    dim3 block_dim = NUM_THREADS;
+    CHECK_CUDA(cudaMemset(m_L1_avg, 0, sizeof(float)));
+    CHECK_CUDA(cudaMemset(m_Ldssim_avg, 0, sizeof(float)));
+    forward_kernel_1<<<num_blocks, block_dim>>>(BCHW, img_pred, img_gt, ssim_map, m_L1_avg, m_Ldssim_avg);
+    forward_kernel_2<<<1, 1>>>(k_lambda, m_L1_avg, m_Ldssim_avg, m_loss);
+    return m_loss;
+}
+
+template <typename T>
+__global__ void fill_kernel(size_t N, T value, T* out_buffer)
+{
+    uint32_t i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i >= N) return;
+    out_buffer[i] = value;
+}
+
+__global__ void backward_kernel( //
+    size_t BCHW,
+    const float* img_pred,
+    const float* img_gt,
+    float lambda,
+    const float* dLdssim_dy,
+    float* out_dL_dy)
+{
+    uint32_t i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i >= BCHW) return;
+    float dif = img_pred[i] - img_gt[i];
+    float sgn = dif > 1e-8f ? 1.0f : (dif < -1e-8f ? -1.0f : 0.0f);
+    // TODO using dLdssim_dy directly, am I ignoring mean?
+    out_dL_dy[i] = (1.0f / float(BCHW)) * (1.0f - lambda) * sgn ;//+ lambda * dLdssim_dy[i]; TODO DEBUG
+}
+
+void GSLoss::backward( //
+    int B,
+    int C,
+    int H,
+    int W,
+    const float* img_pred,
+    const float* img_gt,
+    float* out_dL_dy)
+{
+    size_t BCHW = B * C * H * W;
+    m_dL_dmap.resize(BCHW * sizeof(float));
+    /* Compute dLdssim/dy */
+    // Fill dL/dmap with 1s, so the output of FusedSSIM backward is dLdssim/dy.
+    // This because the input dL/dmap is multiplied to dLdssim/dy because of the reverse-mode auto-diff
+    dim3 num_blocks = div_ceil(BCHW, size_t(NUM_THREADS));
+    dim3 block_dim = NUM_THREADS;
+    fill_kernel<float>
+        <<<num_blocks, block_dim>>>(BCHW, 1.0f, m_dL_dmap.data_ptr<float>()); // TODO fill with lambda (see formulation)
+    float* dLdssim_dy =
+        m_fused_ssim.backward(FUSEDSSIM_C1, FUSEDSSIM_C2, B, C, H, W, img_pred, img_gt, m_dL_dmap.data_ptr<float>());
+    /* Compute dL/dy */
+    backward_kernel<<<num_blocks, block_dim>>>(BCHW, img_pred, img_gt, k_lambda, dLdssim_dy, out_dL_dy);
+}
---
+++ gs_train/gs_loss.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "fused_ssim/ssim_if.h"
+
+namespace gs_train
+{
+class GSLoss
+{
+private:
+    static constexpr float k_lambda = 0.2f;
+
+    FusedSSIM m_fused_ssim;
+
+    Buffer m_tmp_buffer{"gsloss/tmp_buffer"};
+    /* Forward */
+    float* m_L1_avg{};
+    float* m_Ldssim_avg{};
+    /// Forward output
+    float* m_loss{};
+    /* Backward */
+    Buffer m_dL_dmap{"gsloss/dL_dmap"};
+    /// Backward output: gradient of loss w.r.t. image prediction
+    Buffer m_dL_dy{"gsloss/dL_dy"};
+
+public:
+    explicit GSLoss();
+    ~GSLoss() = default;
+
+    /// Run the forward pass for the Gaussian Splatting loss.
+    /// \param B Number of batches
+    /// \param C Number of channels
+    /// \param H Height of the image
+    /// \param W Width of the image
+    /// \param img_pred Image predicted by Gaussian Splatting of shape (B, C, H, W)
+    /// \param img_gt Ground truth image of shape (B, C, H, W)
+    /// \return A device pointer to the loss, a single scalar
+    float* forward(int B, int C, int H, int W, const float* img_pred, const float* img_gt);
+
+    /// Run the backward pass for the Gaussian Splatting loss.
+    /// \param[out] out_dL_dy
+    ///     A device pointer to the gradient of the loss w.r.t. the predicted image
+    void backward(int B, int C, int H, int W, const float* img_pred, const float* img_gt, float* out_dL_dy);
+};
+} // namespace gs_train

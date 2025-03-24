95535178d19ea6e3e07bad59c2e848f2f0cc4018
Lorenzo Rutayisire <loryruta23@gmail.com>
2025-03-24 17:03:06 +0100

    image_fill: add stream

--- recogs/utils/image/image_fill.h
+++ recogs/utils/image/image_fill.h
@@ -17,12 +17,14 @@ __global__ void image_fill_kernel(IMAGE image, typename IMAGE::Value fill_value)
 } // namespace detail
 
 template <int C, typename T, ImageMemoryLayout MEMORY_LAYOUT>
-void image_fill(Image<C, T, MEMORY_LAYOUT>& image, typename Image<C, T, MEMORY_LAYOUT>::Value fill_value)
+void image_fill(Image<C, T, MEMORY_LAYOUT>& image,
+                typename Image<C, T, MEMORY_LAYOUT>::Value fill_value,
+                cudaStream_t stream)
 {
     dim3 num_blocks{};
     num_blocks.x = div_ceil(image.width, 32u);
     num_blocks.y = div_ceil(image.height, 32u);
     dim3 block_dim{32, 32};
-    detail::image_fill_kernel<Image<C, T, MEMORY_LAYOUT>><<<num_blocks, block_dim>>>(image, fill_value);
+    detail::image_fill_kernel<Image<C, T, MEMORY_LAYOUT>><<<num_blocks, block_dim, 0, stream>>>(image, fill_value);
 }
 } // namespace gs_train

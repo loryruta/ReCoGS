c76d39f0a54bc8fbed9a480b12991989b824f169
Lorenzo Rutayisire <loryruta23@gmail.com>
2025-03-24 17:02:19 +0100

    utils: add imgui_utils.h

---
+++ recogs/utils/imgui_utils.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <imgui.h>
+
+#define RCGS_IMGUI_DISABLE_BUTTON(disabled_, code_)                                                                    \
+    do {                                                                                                               \
+        if (!!(disabled_)) ImGui::BeginDisabled();                                                                     \
+        code_;                                                                                                         \
+        if (!!(disabled_)) ImGui::EndDisabled();                                                                       \
+    } while (false)
+
+namespace gs_train
+{
+/// Whether the user is interacting with ImGui UIs (e.g. hovering a window).
+/// In such a case, the interaction event shouldn't be forwarded to, for example, the camera controller.
+inline bool imgui_want_ui_interaction()
+{
+    ImGuiIO& io = ImGui::GetIO();
+    return io.WantCaptureKeyboard || io.WantCaptureMouse;
+}
+
+/// Disable any user interaction about to happen in the current ImGui frame.
+inline void imgui_disable_ui_interaction()
+{
+    ImGuiIO& io = ImGui::GetIO();
+    io.WantCaptureKeyboard = false;
+    io.WantCaptureMouse = false;
+}
+} // namespace gs_train
